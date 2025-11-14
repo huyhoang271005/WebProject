e39de03fe0043b1b8fe624418a5715a764de39d2
Huy Hoàng <huyhoang271005@gmail.com>
2025-11-15 01:29:47 +0700

    huy update

---
+++ public/public.js
@@ -0,0 +1,17 @@
+const iconEye = document.getElementsByClassName('icon-eye');
+const password = document.getElementsByClassName('password');
+for(let i of iconEye){
+    i.addEventListener('click', ()=>{
+        const input = icon.closest('.password-layout').querySelector('.password');
+        if(input.type === 'password'){
+            input.type = 'text';
+            i.classList.remove('fa-eye');
+            i.classList.add('fa-eye-slash');
+        }
+        else {
+            input.type = 'password';
+            i.classList.remove('fa-eye-slash');
+            i.classList.add('fa-eye');
+        }
+    })
+}
