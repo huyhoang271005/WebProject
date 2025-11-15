ced455075414f697c2e4fdf6b840db37fdd85ccc
Huy Hoàng <huyhoang271005@gmail.com>
2025-11-15 14:34:56 +0700

    huy update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const rememberUser = localStorage.getItem('rememberUser');
 
 if(rememberUser === 'true'){
-    window.location.replace("./auth/login");
+    window.location.replace("./pages/home");
 }
 else {
-    window.location.replace("./auth/register");
+    window.location.replace("./auth/login");
 }
---
+++ pages/js/profile.js
@@ -0,0 +1,39 @@
+import { callAPI } from "../../public/api.js";
+import { showDialog } from "../../dialog/dialog.js";
+const usernameInput = document.getElementById('username');
+const fullNameInput = document.getElementById('fullName');
+const birthdayInput = document.getElementById('birthday');
+const genderInput = document.getElementById('gender');
+const emailInput = document.getElementById('email');
+const role = document.getElementById('role');
+const saveBtn = document.getElementById('saveBtn');
+const createdAt = document.getElementById('createdAt');
+const updatedAt = document.getElementById('updatedAt');
+const avatarInput = document.getElementById('avatar');
+const avatarPreview = document.getElementById('avatarPreview');
+let avatarId = null;
+async function loadProfile() {
+    const result = await callAPI('/profile');
+    if(!profile.success){
+        showDialog('error', result.message);
+        return;
+    }
+    const profile = result.data;
+    avatarPreview.src = profile.imageUrl
+    avatarId = profile.imageId;
+    usernameInput.textContent = profile.username;
+    fullNameInput.textContent = profile.fullName;
+    birthdayInput.textContent = profile.birthday;
+    genderInput.textContent = profile.gender;
+    emailInput.textContent = profile.emails[0].email;
+    role.textContent = profile.role;
+    createdAt = profile.createdAt;
+    updatedAt = profile.updatedAt;
+}
+await loadProfile();
+avatarInput.addEventListener('change', (e) => {
+    const file = e.target.files[0];
+    if(file){
+        avatarPreview.src = URL.createObjectURL(file);
+    }
+});
