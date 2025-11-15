306252074f585417b346ec1fe1f252a8f77b2347
Huy Hoàng <huyhoang271005@gmail.com>
2025-11-15 13:17:14 +0700

    huy update

---
+++ index.js
@@ -0,0 +1,8 @@
+const rememberUser = localStorage.getItem('rememberUser');
+
+if(rememberUser === 'true'){
+    window.location.replace("/auth/login");
+}
+else {
+    window.location.replace("/auth/register");
+}
