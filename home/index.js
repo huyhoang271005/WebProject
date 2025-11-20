161369b083af0dbb9afb68843f418beb894b2049
Huy Hoàng <huyhoang271005@gmail.com>
2025-11-21 03:44:24 +0700

    huy upate

--- home/index.js
+++ home/index.js
@@ -1,5 +1,6 @@
 const btn = document.getElementById('test');
 btn.style.position = 'absolute';
+btn.style.width = '';
 btn.addEventListener('mouseover', ()=>{
     btn.style.left = Math.random()*90+1 + 'vw';
     btn.style.top = Math.random()*90+1 + 'vh';
