786936feafc0b6abe39c558336bbb7c59436cd14
Huy Hoàng <huyhoang271005@gmail.com>
2025-11-21 17:00:53 +0700

    huy update

--- public/public.js
+++ public/public.js
@@ -18,6 +18,7 @@ export async function getLoader(idLayout, callback) {
         await callback();
         loader.classList.remove('loading');
         iconLoader.style.display = 'none';
+        callback = null;
     }
 }
 
