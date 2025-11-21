86b8276a08cad59f261a26b26fd5f379a7e6769a
Huy Hoàng <huyhoang271005@gmail.com>
2025-11-21 23:15:11 +0700

    huy update

--- public/public.js
+++ public/public.js
@@ -4,15 +4,14 @@
 export async function getLoader(idLayout, callback) {
     if(callback && typeof callback === 'function'){
         const loader = document.getElementById(idLayout);
-        async () => {
+        await (async () => {
             if (loader.querySelector('.icon-loader')) return;
 
             const response = await fetch("/WebProject/public/icon-loader.html");
             const html = await response.text();
             loader.insertAdjacentHTML("beforeend", html);
-        }
+        })();
         const iconLoader = loader.querySelector('.icon-loader');
-        iconLoader.style.display = 'none';
         iconLoader.style.display = 'inline-block';
         loader.classList.add('loading');
         await callback();
