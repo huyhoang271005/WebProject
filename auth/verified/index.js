e440f33d32b4be09569bc35b2edec8d563400c84
Huy Hoàng <huyhoang271005@gmail.com>
2025-11-21 23:57:53 +0700

    huy update

--- auth/verified/index.js
+++ auth/verified/index.js
@@ -1,14 +1,15 @@
 import { callAPI } from "../../public/api.js";
 
 (async () => {
-  const params = new URLSearchParams(window.location.search);
-  const token = params.get("token");
+  const path = window.location.pathname;
+  const parts = path.split('/').filter(Boolean);
+  const token = parts[parts.length - 1];
   const h1 = document.getElementById("status");
   const image = document.getElementById("image");
   const back = document.getElementById("back");
 
   h1.style.display = 'block';
-  const result = await callAPI(`/auth/verify?token=${token}`);
+  const result = await callAPI(`/auth/verify/${token}`);
   back.style.display = "inline-block";
   h1.classList.add(result.success ? 'success' : 'error')
   image.src = result.success ? "https://cdn-icons-png.flaticon.com/512/190/190411.png"
