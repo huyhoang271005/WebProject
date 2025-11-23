ac6b56d91eb678e5e9f02fd5d8d82c774cdf49b7
Huy Hoàng <huyhoang271005@gmail.com>
2025-11-23 22:23:00 +0700

    huy update

--- home/index.js
+++ home/index.js
@@ -5,3 +5,8 @@ btn.addEventListener('mouseover', ()=>{
     btn.style.left = Math.random()*90+1 + 'vw';
     btn.style.top = Math.random()*90+1 + 'vh';
 });
+const logout = document.getElementById('logout');
+logout.addEventListener('click', ()=>{
+    localStorage.setItem('rememberUser', 'false');
+    window.location.replace = "../auth/login"
+});
