f06c66281c277a8778674aa2164ded64d6b8498c
Huy Hoàng <huyhoang271005@gmail.com>
2025-11-23 17:20:16 +0700

    huy update

--- public/api.js
+++ public/api.js
@@ -69,7 +69,7 @@ async function refreshAccessToken() {
             "Device-type": "WEB"
         }
     });
-    if(!res.success) {
+    if(res.status === 401) {
         setTimeout(()=>{
             window.location.replace('/WebProject/auth/login');
         }, 5000);
