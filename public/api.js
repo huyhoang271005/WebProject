76d3dd6cdadc797e761e47636b40e083482a02a8
Huy Hoàng <huyhoang271005@gmail.com>
2025-11-30 01:39:59 +0700

    test

--- public/api.js
+++ public/api.js
@@ -108,6 +108,9 @@ export async function connectSse(endpoint, callback) {
                     return;
                 }
                 accessToken = result.data.accessToken;
+                es.close();
+                start();
+                return;
             }
         }
     });
