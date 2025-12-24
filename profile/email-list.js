5cffd328e89616dc1d9ef2261edfdbc54655c208
Huy Hoàng <huyhoang271005@gmail.com>
2025-12-24 11:28:37 +0700

    huy fix bug

--- profile/email-list.js
+++ profile/email-list.js
@@ -73,7 +73,8 @@ export function initEmailList(initialEmails = []) {
                     if (addEmail.success){
                         const result = await callAPI('/auth/send-verify-email', 'POST', {email: email.email});
                         if(result.success){
-                            email.validated = false;
+                            email.emailId = result.data.emailId;
+                            email.validated = result.data.validated;
                             render();
                         }
                         await showDialog(result.success ? 'success' : 'error', result.message);
